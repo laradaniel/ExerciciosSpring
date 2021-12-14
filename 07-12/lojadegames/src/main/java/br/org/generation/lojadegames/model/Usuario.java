c19e3fb9ea6c18eda6a92e1066953520d0fcb059
Lara Daniel <laradanielt@gmail.com>
2021-12-14 13:00:12 -0300

    Implementação Security

---
+++ 07-12/lojadegames/src/main/java/br/org/generation/lojadegames/model/Usuario.java
@@ -0,0 +1,80 @@
+package br.org.generation.lojadegames.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank (message = "O atributo é obrigatório")
+	private String nome;
+	
+	@NotBlank (message = "O atributo é obrigatório")
+	@Email
+	private String usuario;
+	
+	@NotBlank (message = "O atributo é obrigatório")
+	private String senha;
+	
+	@Column(name = "data_nascimento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	@NotNull(message = "O atributo Data de Nascimento é obrigatório")
+	private LocalDate dataNascimento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
